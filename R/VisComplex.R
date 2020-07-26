1d46aa40c73028d525de166d77dfcb96ae6075ee
PeterBiber <castor.fiber@gmx.de>
2020-07-26 15:18:51 +0200

    Make improved function call work under windows
    compFun had to be handed over explictly to the foreach loop.

--- R/VisComplex.R
+++ R/VisComplex.R
@@ -1166,7 +1166,7 @@ phasePortrait <- function(FUN, moreArgs = NULL, xlim, ylim,
        cat("parallel loop starting ... ")
 
        w <- foreach(i = c(1:length(z)), .combine = rbind,
-                    .export = c("invertFlip")) %dopar% {
+                    .export = c("invertFlip", "compFun")) %dopar% {
          # Make sure dimensions are correct, because
          # one-line arrays can become vectors mysteriously ...
          if(length(dim(z[[i]])) < 2) dims <- c(1, length(z[[i]]))
