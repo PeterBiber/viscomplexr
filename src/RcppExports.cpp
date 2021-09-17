bbbe732f5bfa97b3ae3b4e6bc9fdad1205d3a55e
PeterBiber <castor.fiber@gmx.de>
2021-09-17 20:10:40 +0200

    Automatic update of RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // mandelbrot
 std::complex<double> mandelbrot(std::complex<double> z, int itDepth);
 RcppExport SEXP _viscomplexr_mandelbrot(SEXP zSEXP, SEXP itDepthSEXP) {
